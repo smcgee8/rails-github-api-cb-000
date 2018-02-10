f6739b38021c26ce6cd3af340a6ce89d9c0e4dab
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 16:21:36 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       code: params[:code],
       redirect_uri: "http://67.205.130.239:57441/auth"
     }
+    raise req_params.to_json
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
       req.body = req_params.to_json
     end
