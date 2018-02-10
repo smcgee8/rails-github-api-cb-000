0cfb0e0dd3dba7109cb587c580f01878f414750a
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 15:56:01 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
     end
     body = JSON.parse(resp.body)
     session[:token] = body["access_token"]
+    binding.pry
     redirect_to root_path
   end
 end
